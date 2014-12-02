c678b9b1f489a41c2e272115dd5ca3f2e4b13a9b
sydul <sydulislam1980@yahoo.com>
2014-12-01 21:23:27 -0500

    bangla

---
+++ ClassdDce01/src/Manmesbaik.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Manmesbaik {
+
+	public static void main(String[] args) 
+	{
+	FirefoxDriver wal = new FirefoxDriver();
+		wal.get("http://www.Walgreens.com");
+
+	}
+
+}
